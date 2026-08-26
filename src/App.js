5b1405f343a0d52750878a546eafd75e87d7a470
Reshmi <reshmi@Reshmis-MacBook-Neo.local>
2026-08-26 14:06:10 +0530

    Create ShopEase initial layout

--- src/App.js
+++ src/App.js
@@ -1,24 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import "./App.css";
+
+import Products from "./pages/Products";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Cart from "./pages/Cart";
+
+function Home() {
+  return (
+    <section className="hero">
+      <div className="hero-content">
+        <p className="small-title">WELCOME TO SHOPEASE</p>
+
+        <h1>
+          Shop Smart.
+          <br />
+          Live Better.
+        </h1>
+
+        <p className="hero-text">
+          Discover amazing products at the best prices.
+        </p>
+
+        <button>Shop Now →</button>
+      </div>
+    </section>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="app">
+
+        <nav className="navbar">
+          <Link to="/" className="logo">
+            ShopEase
+          </Link>
+
+          <div className="nav-links">
+            <Link to="/">Home</Link>
+            <Link to="/products">Products</Link>
+            <Link to="/about">About</Link>
+            <Link to="/contact">Contact</Link>
+            <Link to="/cart">🛒 Cart</Link>
+          </div>
+        </nav>
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/cart" element={<Cart />} />
+        </Routes>
+
+      </div>
+    </BrowserRouter>
   );
 }
 
